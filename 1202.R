2898dda1f37398fbe75672e2b309f5074a091d3d
ducktsai <609880041@gms.tku.edu.tw>
2020-12-08 16:42:55 +0800

    1208

--- 1202.R
+++ 1202.R
@@ -6,8 +6,9 @@ library(magrittr)
 library(tidyr)
 BBS<-fread("C:/mycode/1202/occurrence.txt",sep="\t",encoding="UTF-8")
 str(BBS)
-#BBS %<>% Filter(function(x)!all(is.na(x)), .)
+BBS %<>% Filter(function(x)!all(is.na(x)), .)
 #=BBS<-BBS%>%Filter(function(x)!all(is.na(x),BBS))
 #'F'ilter大寫,%<>%右邊做完丟回左邊=BBS<-%>%...,!等於not,all(is.na(x),BBS) 先判定欄位中是否為NA,加!表示不是NA的值,再過濾留下
-is.na(BBS)
-
+a<-BBS %>%
+  select(where(~!all(is.na(.))))
+  
